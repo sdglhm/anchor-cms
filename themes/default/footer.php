c587f97910b3418c134eeb642273a767fe924491
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2016-04-04 15:05:52 +0530

    Update footer.php

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -8,8 +8,6 @@
 	                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
 	                    <?php endif; ?>
 
-	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
-
 	                    <li><a href="<?php echo base_url(); ?>" title="Return to my website.">Home</a></li>
 	                </ul>
 	            </footer>
